e61ee5531496a0ccc56b1865065c7ff998395262
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-10-22 13:48:30 +0200

    Add absorbance models and migrations to the 'data' app
    Introduced models for AbsorbanceProperties, AbsorbanceData, AbsorbanceLocalMaximum, and Sample. Added the corresponding migration file to support data ingestion and analysis workflows. Updated UploadedFile model to include new process method option.

--- data/models.py
+++ data/models.py
@@ -45,3 +45,27 @@ class UploadedFile(models.Model):
         ordering = ['-uploaded_at']
 
 UploadedFile._meta.get_field("process_method").choices = UploadedFile.get_process_method_choices()
+
+class Sample(models.Model):
+    name = models.CharField(max_length=255)
+    uploaded_file = models.ForeignKey(UploadedFile, on_delete=models.CASCADE)
+    date = models.DateField()
+
+class AbsorbanceProperties(models.Model):
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    wave_start = models.FloatField(default=400.0)
+    wave_end = models.FloatField(default=750.0)
+
+class AbsorbanceLocalMaximum(models.Model):
+    absorbance_properties = models.ForeignKey(AbsorbanceProperties, on_delete=models.CASCADE)
+    wave_start = models.FloatField()
+    wave_end = models.FloatField()
+
+class AbsorbanceData(models.Model):
+    absorbance_properties = models.ForeignKey(AbsorbanceProperties, on_delete=models.CASCADE)
+    wave_nm = models.FloatField()
+    absorbance_au = models.FloatField()
+    dark_counts = models.FloatField()
+    ref_counts = models.FloatField()
+    sample_counts = models.FloatField()
+
